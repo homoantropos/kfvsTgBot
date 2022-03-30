072d9edc747c790fa5d92ac63f0423be86262761
Sergii Antropov <homoantropos@gmail.com>
2022-03-30 13:10:11 +0300

    initial comit

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Telegram = require('node-telegram-bot-api');
-const bot = new Telegram();
+const keys = require('./config/keys');
+
+const bot = new Telegram(keys.token, {polling: true});
 
 
 module.exports = bot
