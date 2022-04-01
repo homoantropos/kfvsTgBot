788e0d7a0d25a478a14f2f77405a89bffbecb709
Sergii Antropov <homoantropos@gmail.com>
2022-04-01 14:36:34 +0300

    initial comit

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.json());
 
 bot.launch();
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 module.exports = app
