70d4110c2592441a7bba984b3b4c8745df2ab7fb
Segii Antropov <homoantropos@gmail.com>
2022-04-01 20:35:42 +0300

    router added

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 const bot = require('./bot');
 const bodyParser = require("body-parser");
 
-
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.json());
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const app = require('./app');
 const port = process.env.PORT || 3000
+const express = require('express');
+const router = express.Router();
 
 app.listen(port, () => {
     console.log(`Server is running on ${port} port`)
-})
+});
+
+setTimeout(
+    () => router.get('https://kfvstgbot.herokuapp.com/'), 1000*60*10
+);
 
