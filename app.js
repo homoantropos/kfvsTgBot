ccd1b4bc855c4030d0d952f365d531708de79c51
Sergii Antropov <homoantropos@gmail.com>
2022-04-01 11:07:44 +0300

    initial comit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const bot = require('./bot');
+const bodyParser = require("body-parser");
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(bodyParser.json());
 
 bot.launch();
 
