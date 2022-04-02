01bd8796ce24f8bd6154ef9fad953bd52c35e546
Segii Antropov <homoantropos@gmail.com>
2022-04-02 16:11:41 +0300

    router added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,9 +2,13 @@ class User_controller {
 
     async createUser( req, res) {
         try {
-
+            res.status(200).json({
+                message: 'Successfully created'
+            })
         } catch(error) {
-
+            res.status(500).json({
+                message: error.message ? error.message : error
+            })
         }
     }
 
