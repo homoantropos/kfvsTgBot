d26fdb59e98b52bb8822876b6bf99474101e5e35
Segii Antropov <homoantropos@gmail.com>
2022-04-02 16:23:00 +0300

    router added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,11 +1,24 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 class User_controller {
 
-    async createUser( req, res) {
+    async createUser(req, res) {
         try {
-            res.status(200).json({
-                message: 'Successfully created'
-            })
-        } catch(error) {
+            const salt = await bcrypt.genSalt(10);
+            let password = await bcrypt.hash(req.body.password, salt);
+            await User.findOrCreate({
+                where:
+                    {
+                        email: req.body.email,
+                        password
+                    }
+            });
+            const user = await User.findOne({
+                where: {email: req.body.email}
+            });
+            res.status(201).json(user);
+        } catch (error) {
             res.status(500).json({
                 message: error.message ? error.message : error
             })
