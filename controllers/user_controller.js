89288d646b0f44c78ca6ab020c98f2be5d4d0598
Segii Antropov <homoantropos@gmail.com>
2022-04-03 19:39:09 +0300

    router added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,26 +4,33 @@ const User = require('../models/User');
 class User_controller {
 
     async createUser(req, res) {
-        try {
-            const salt = await bcrypt.genSalt(10);
-            let password = await bcrypt.hash(req.body.password, salt);
-            await User.findOrCreate({
-                where:
-                    {
-                        email: req.body.email,
-                        role: req.body.role,
-                        password
-                    }
-            });
-            const user = await User.findOne({
-                where: {email: req.body.email}
-            });
-            res.status(201).json(user[0]);
-        } catch (error) {
+        const candidate = await User.findOne({
+            where: {email: req.body.email}
+        });
+        if(!candidate) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                let password = await bcrypt.hash(req.body.password, salt);
+                const user = await User.findOrCreate({
+                    where:
+                        {
+                            email: req.body.email,
+                            role: req.body.role,
+                            password
+                        }
+                });
+                res.status(201).json(user[0]);
+            } catch (error) {
+                res.status(500).json({
+                    message: error.message ? error.message : error
+                })
+            }
+        } else {
             res.status(500).json({
-                message: error.message ? error.message : error
+                message: 'Така адреса електронної пошти взе зайнята'
             })
         }
+
     }
 
     async getAllUsers (req, res) {
