c2e930c4fbc10a0eb47ff2f6cc4bf435e18f5c4e
Sergii Antropov <homoantropos@gmail.com>
2022-04-05 14:44:28 +0300

    initial comit

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -86,6 +86,7 @@ class User_controller {
 
     async sentLinkToResetPassword(req, res) {
         try {
+            console.log(process.env);
             const candidate = await User.findOne({
                 where: {email: req.body.email}
             })
