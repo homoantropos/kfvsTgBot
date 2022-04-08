60fd1f404f347ce538102694385aed6c7c28244c
Sergii Antropov <homoantropos@gmail.com>
2022-04-08 16:10:22 +0300

    User, Subscriber, Occasion models added

--- controllers/onMessage.js
+++ controllers/onMessage.js
@@ -241,7 +241,7 @@ module.exports = async ctx => {
                 occasions.map(
                     occasion => {
                         const occasionName = occasion.name;
-                        ctx.reply(occasion.date.toLocaleDateString(),
+                        ctx.reply(occasion.date.toLocaleDateString('uk-UK'),
                             {reply_markup: {inline_keyboard: keyboardsFactory.provide(occasionName)}}
                         );
                     }
