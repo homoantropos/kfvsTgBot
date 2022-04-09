fe87984f480f53b6148674d36ad5d17869a9409e
Segii Antropov <homoantropos@gmail.com>
2022-04-09 20:28:28 +0300

    maxSubsNumber added to occasion model

--- controllers/onMessage.js
+++ controllers/onMessage.js
@@ -51,7 +51,7 @@ module.exports = async ctx => {
         case('заходи') :
             ctx.reply(
                 'Заходи 2022 року, оберіть місяць',
-                {reply_markup: {keyboard: keyboardsFactory.schedule()}});
+                {reply_markup: {keyboard: keyboards.shedule}});
             break;
 
         case('фізична культура') :
