b1f5a67b747b8d1f8cd4fa57ca79fb631c21df8c
Segii Antropov <homoantropos@gmail.com>
2022-04-09 20:23:28 +0300

    maxSubsNumber added to occasion model

---
+++ config/context.js
@@ -0,0 +1,43 @@
+Context {
+    update: {
+        update_id: 387156431,
+            message: {
+            message_id: 22,
+                from: {
+                    id: 481547986,
+                    is_bot: false,
+                    first_name: 'Sergii',
+                    last_name: 'Antropov',
+                    username: 'homo_antropos',
+                    language_code: 'uk'
+                },
+                chat: {
+                    id: 481547986,
+                    first_name: 'Sergii',
+                    last_name: 'Antropov',
+                    username: 'homo_antropos',
+                    type: 'private'
+                },
+                date: 1648920613,
+                text: 'п'
+        }
+    },
+    tg: Telegram {
+        token: '',
+            response: undefined,
+            options: {
+            agent: [Agent],
+                attachmentAgent: undefined
+        }
+    },
+    botInfo: {
+        id: 5183317981,
+            is_bot: true,
+            first_name: 'HomoAntropos',
+            username: 'HomoAntropos_bot',
+            can_join_groups: true,
+            can_read_all_group_messages: false,
+            supports_inline_queries: false
+    },
+    state: {}
+}
--- utils/keyboardsFactory.js
+++ utils/keyboardsFactory.js
@@ -1,3 +1,5 @@
+const months = require('../config/months');
+
 exports.provide = (option) => {
     return [
         [
@@ -8,3 +10,27 @@ exports.provide = (option) => {
         ]
     ]
 }
+
+exports.schedule = () => {
+    const today = new Date();
+    let monthKeyboard = [];
+    const currentMonths = months.filter(month => month >= today.getMonth());
+    let row = [];
+    currentMonths.map(
+        month => {
+            row.push(
+                {
+                    text: `${month}`,
+                    callback_data: `${month}`
+                }
+            );
+            if(row.length === 3) {
+                monthKeyboard.push(row);
+                row = row.splice(0);
+            }
+        }
+    )
+    return monthKeyboard;
+}
+
+
