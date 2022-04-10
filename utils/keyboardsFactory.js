20f91e14fe6347323e2124995e2bc5e8b2edacf9
Segii Antropov <homoantropos@gmail.com>
2022-04-10 19:24:34 +0300

    maxSubsNumber added to occasion model

--- utils/keyboardsFactory.js
+++ utils/keyboardsFactory.js
@@ -13,7 +13,7 @@ exports.provide = (option) => {
 
 exports.schedule = () => {
     let monthKeyboard = [];
-    const currentMonths = months.filter(month => months.indexOf(month) >= 5);
+    const currentMonths = months.filter(month => months.indexOf(month) >= 6);
     const rowsCount = Math.ceil((currentMonths.length/3));
     let row = [];
     currentMonths.map(
