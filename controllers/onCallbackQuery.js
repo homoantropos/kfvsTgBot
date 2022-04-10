4a42ffed2796ee08442a619e0ad8d0d7f22d07c8
Segii Antropov <homoantropos@gmail.com>
2022-04-10 18:55:35 +0300

    maxSubsNumber added to occasion model

--- controllers/onCallbackQuery.js
+++ controllers/onCallbackQuery.js
@@ -52,7 +52,6 @@ module.exports = async ctx => {
                 });
                 if(occasion) {
                     ctx.reply(occasion.description, {parse_mode: 'HTML'});
-                    ctx.reply(' ', {reply_markup: {inline_keyboard: inlineKBRDS.toStart}});
                 } else {
                     ctx.reply(
                         `Вибачте, такої команди не виявлено, спробуйте повернутися до початку роботи і перевірити вірність введених даних або скористатися кнопокю "Контакти" для звязку з нами`,
