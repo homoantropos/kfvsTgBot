232e73aa421f9c1a1364d587d7f3b271f2f53432
Segii Antropov <homoantropos@gmail.com>
2022-04-10 21:28:49 +0300

    maxSubsNumber added to occasion model

--- routes/subscriber_routes.js
+++ routes/subscriber_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const controller = require('../controllers/subscriber_controller');
-const bot = require('../bot');
+const bot = require('bot');
 
 router.post('/create', passport.authenticate('jwt', {session: false}), controller.createSubscriber);
 router.patch('/:id', passport.authenticate('jwt', {session: false}), controller.updateSubscriber);
