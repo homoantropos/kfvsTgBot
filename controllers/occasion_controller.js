70113f30fb46ad5db44fec841d84272f9dde6445
Segii Antropov <homoantropos@gmail.com>
2022-04-11 21:46:58 +0300

    maxSubsNumber added to occasion model

--- controllers/occasion_controller.js
+++ controllers/occasion_controller.js
@@ -118,7 +118,7 @@ class Occasion_controller {
 
     async addSubscriber(req, res) {
         try {
-            console.log(req.query.subscriberId);
+            console.log('req.query.subscriberId: ', req.query.subscriberId);
             const occasion = await Occasion.scope('occasion').findOne({
                 where: {
                     id: req.params.id
