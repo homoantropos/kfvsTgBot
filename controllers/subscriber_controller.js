16c785a29eed56feadd3eb5b8e816faafd917ce9
Sergii Antropov <homoantropos@gmail.com>
2022-04-12 12:06:11 +0300

    User, Subscriber, Occasion models added

--- controllers/subscriber_controller.js
+++ controllers/subscriber_controller.js
@@ -86,7 +86,10 @@ class Subscriber_controller {
         try {
             let subscribers = await Subscriber.scope('subs').findAll({include: {model: Occasion, as: 'occasions'}});
             if (req.query.occasionId) {
-                const occasion = await Occasion.findAll({include: {model: Subscriber, as: 'subscribers'}});
+                const occasion = await Occasion.findAll({
+                    include: {model: Subscriber, as: 'subscribers'},
+                    where: {id: req.query.occasionId}
+                });
                 subscribers = occasion.subscribers.slice();
             }
             res.status(200).json(subscribers);
