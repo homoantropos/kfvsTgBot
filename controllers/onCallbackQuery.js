3e95d0f4bafce1e400223094aafa694456407329
Segii Antropov <homoantropos@gmail.com>
2022-04-12 20:31:47 +0300

    maxSubsNumber added to occasion model

--- controllers/onCallbackQuery.js
+++ controllers/onCallbackQuery.js
@@ -52,6 +52,7 @@ module.exports = async ctx => {
                     include: {model: Subscriber, as: 'subscribers'},
                     where: {name: data}
                 });
+                console.log('CHEK: ', checkSubs(occasion, id));
                 if (occasion) {
                     await ctx.reply(occasion.description, {parse_mode: 'HTML'});
                     if(checkSubs(occasion, id)) {
