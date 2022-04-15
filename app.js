84df97206a3d2193f6f244e1aa8c9ffb3b2991df
Segii Antropov <homoantropos@gmail.com>
2022-04-15 20:26:24 +0300

    maxSubsNumber added to occasion model

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const passport = require('passport');
+const multer = require('multer');
 
 const sequelize = require('./database/sequelize');
 
@@ -14,14 +15,14 @@ const Subscriber = require("./models/Subscriber");
 
 bot.listen();
 
+upld = multer({dest: 'uploads/'});
+
 app.use(passport.initialize());
 require('./middleware/passport')(passport);
 app.use(require('morgan')('dev'));
 app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(require('cors')());
 
 app.use('/api/user', userRoutes);
@@ -35,34 +36,35 @@ sequelize.sync({alter: true})
     (err) => console.log(err)
 )
 
-app.use(express.Router().post('/api/send', passport.authenticate('jwt', {session: false}),
+app.use(upld.single(), express.Router().post('/api/send', passport.authenticate('jwt', {session: false}),
     async (req, res) => {
-    try {
-        if(req.body.tgIds.length > 0) {
-            req.body.tgIds.map(
-                async tgId => {
-                    let subscriber = await Subscriber.scope('subs').findOne({
-                        where: {tgId}
-                    });
-                    if (subscriber) {
-                        await bot.bot.telegram[req.body.method](subscriber.tgId, req.body?.text, {parse_mode: 'HTML'}, req?.file);
+        try {
+            const tgIds = JSON.parse(req.body.tgIds);
+            if (tgIds.length > 0) {
+                tgIds.map(
+                    async tgId => {
+                        let subscriber = await Subscriber.scope('subs').findOne({
+                            where: {tgId}
+                        });
+                        if (subscriber) {
+                            await bot.bot.telegram[req.body.method](subscriber.tgId, req.body?.text, {parse_mode: 'HTML'}, req?.file);
+                        }
                     }
-                }
-            );
-        } else {
-            const subscribers = await Subscriber.scope('subs').findAll();
-            subscribers.map(
-                subscriber => bot.bot.telegram[req.body.method](subscriber.tgId, req.body.text, {parse_mode: 'HTML'})
-            )
+                );
+            } else {
+                const subscribers = await Subscriber.scope('subs').findAll();
+                subscribers.map(
+                    subscriber => bot.bot.telegram[req.body.method](subscriber.tgId, req.body.text, {parse_mode: 'HTML'})
+                )
+            }
+            res.status(200).json({
+                message: 'Повідомлення успішно відправлено'
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: error.message ? error.message : error
+            })
         }
-        res.status(200).json({
-            message: 'Повідомлення успішно відправлено'
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message ? error.message : error
-        })
-    }
-}));
+    }));
 
 module.exports = app
