9e527fbb8b705aebb26f7782e28e1958508d62e6
Segii Antropov <homoantropos@gmail.com>
2022-04-17 17:58:13 +0300

    messages sending options removed to front

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const bot = require('./bot');
 const userRoutes = require('./routes/user_routes');
 const subscriberRoutes = require('./routes/subscriber_routes');
 const occasionRoutes = require('./routes/occasion_routes');
-const messageRoutes = require('./routes/message_routes');
-const Subscriber = require("./models/Subscriber");
 
 bot.listen();
 
@@ -37,47 +35,6 @@ sequelize.sync({alter: true})
         () => console.log('DataBase connection established successfully.')
     ).catch(
     (err) => console.log(err)
-)
-
-app.use(express.Router().post('/api/sendMedia', upload.none(), passport.authenticate('jwt', {session: false}),
-    async (req, res) => {
-        try {
-            if (req.body.tgIds.length > 0) {
-                req.body.tgIds.map(
-                    async tgId => {
-                        let subscriber = await Subscriber.scope('subs').findOne({
-                            where: {tgId}
-                        });
-                        if (subscriber) {
-                            const caption = req.body.text;
-                            switch(req.body.method) {
-                                case('sendPhoto') :
-                                    await bot.bot.telegram.sendPhoto(subscriber.tgId, req.body.mediaUrl, caption);
-                                    break
-                                case('sendVideo') :
-                                    await bot.bot.telegram.sendVideo(subscriber.tgId, req.body.mediaUrl, caption);
-                                    break
-                                default :
-                                    await bot.bot.telegram.sendMessage(subscriber.tgId, caption, {parse_mode: 'HTML'});
-                            }
-                        }
-                    }
-                );
-            }
-            else {
-                const subscribers = await Subscriber.scope('subs').findAll();
-                subscribers.map(
-                    subscriber => bot.bot.telegram[req.body.method](subscriber.tgId, req.body.text, {parse_mode: 'HTML'})
-                )
-            }
-            res.status(200).json({
-                message: 'Повідомлення успішно відправлено'
-            })
-        } catch (error) {
-            res.status(500).json({
-                message: error.message ? error.message : error
-            })
-        }
-    }));
+);
 
 module.exports = app
